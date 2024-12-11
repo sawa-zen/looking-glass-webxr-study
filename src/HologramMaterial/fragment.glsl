b432cd725be45e25427319d039672abcbe234b04
sawa-zen <sawadasuiren@gmail.com>
2024-12-11 10:06:13 +0900

    refactor

--- src/HologramMaterial/fragment.glsl
+++ src/HologramMaterial/fragment.glsl
@@ -32,11 +32,14 @@ vec3 hsv2rgb(vec3 c) {
   return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 }
 
-// RGBからHSVへの変換
-vec3 rgb2hsv(vec3 c) {
+/**
+ * RGBからHSVへの変換
+ * @param {vec3} c RGB
+ */
+vec3 rgb2hsv(vec3 rgb) {
   vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-  vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-  vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+  vec4 p = mix(vec4(rgb.bg, K.wz), vec4(rgb.gb, K.xy), step(rgb.b, rgb.g));
+  vec4 q = mix(vec4(p.xyw, rgb.r), vec4(rgb.r, p.yzx), step(p.x, rgb.r));
 
   float d = q.x - min(q.w, q.y);
   float e = 1.0e-10;
